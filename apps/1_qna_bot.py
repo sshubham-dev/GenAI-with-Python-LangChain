f16412c5e8af3fc289b67f6c1cc615f7f04197c3
SShubham kumar <shubhamkrg1819@gmail.com>
2026-04-05 22:37:00 +0530

    genai code push to github

---
+++ apps/1_qna_bot.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+# que = "Who is PM of India?"
+# result = llm.invoke(que)
+# print(result.content)
+
+while True:
+    query = input("User: ")
+    result = llm.invoke(query)
+    print(result.content)
