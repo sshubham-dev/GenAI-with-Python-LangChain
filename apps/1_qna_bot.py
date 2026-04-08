b4b1b9a515788535b491f0ab3db6173a4d439f1d
SShubham kumar <shubhamkrg1819@gmail.com>
2026-04-08 23:05:59 +0530

    GenAI series, basic QnA bot with llm, streamlit

--- apps/1_qna_bot.py
+++ apps/1_qna_bot.py
@@ -2,18 +2,43 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+
+
+st.title("🤖 AskBuddy - AI QnA Bot")
+st.markdown("My QnA Bot with Langchain and Google Gemini !")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    role = message["role"]
+    content = message["content"]
+    st.chat_message(role).markdown(content)
+
+
+query = st.chat_input("Ask anything ?")
+if query:
+    st.session_state.messages.append({"role":"user", "content":query})
+    st.chat_message("user").markdown(query)
+    res = llm.invoke(query)
+    st.chat_message("ai").markdown(res.content)
+    st.session_state.messages.append({"role":"ai", "content":res.content})
+
+
+
 
 # que = "Who is PM of India?"
 # result = llm.invoke(que)
 # print(result.content)
 
-while True:
-    query = input("User: ")
-    if query.lower() in ["quit","exit","bye"]:
-        print("GoodBye")
-        break
+# while True:
+# query = input("Ask anything ?")
+    # if query.lower() in ["quit","exit","bye"]:
+    #     print("GoodBye")
+    #     break
 
-    result = llm.invoke(query)
-    print(result.content)
+    # result = llm.invoke(query)
+    # print(result.content)
